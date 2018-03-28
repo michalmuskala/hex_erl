bae3ef6362df27b563a9ec90770fc33a392272d9
Wojtek Mach <wojtek@wojtekmach.pl>
2018-03-28 23:16:25 +0200

    Initial work on hex_api

--- test/support/hex_http_test.erl
+++ test/support/hex_http_test.erl
@@ -2,6 +2,7 @@
 -behaviour(hex_http).
 -export([get/2]).
 -define(TEST_REPO_URI, "https://repo.test").
+-define(TEST_API_URI, "https://api.test").
 -define(PRIVATE_KEY, element(2, file:read_file("test/fixtures/test_priv.pem"))).
 -define(PUBLIC_KEY, element(2, file:read_file("test/fixtures/test_pub.pem"))).
 
@@ -15,6 +16,8 @@ get(URI, Headers) when is_binary(URI) and is_map(Headers) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+%% Repo API
 
 fixture(<<?TEST_REPO_URI, "/names">>) ->
     Names = #{
@@ -62,6 +65,28 @@ fixture(<<?TEST_REPO_URI, "/tarballs/ecto-1.0.0.tar">>) ->
 
 fixture(<<?TEST_REPO_URI, _/binary>>) ->
     {ok, {403, #{}, <<"not found">>}};
+
+%% HTTP API
+
+fixture(<<?TEST_API_URI, "/users/josevalim">>) ->
+    Payload = #{
+        <<"username">> => <<"josevalim">>,
+        <<"packages">> => []
+    },
+    {ok, {200, [], term_to_binary(Payload)}};
+
+fixture(<<?TEST_API_URI, "/packages/ecto">>) ->
+    Payload = #{
+        <<"name">> => <<"ecto">>,
+        <<"releases">> => []
+    },
+    {ok, {200, [], term_to_binary(Payload)}};
+
+fixture(<<?TEST_API_URI, "/packages/ecto/releases/1.0.0">>) ->
+    Payload = #{
+        <<"version">> => <<"1.0.0">>
+    },
+    {ok, {200, [], term_to_binary(Payload)}};
 
 fixture(URI) ->
     error({no_fixture, URI}).
